9c578008814601cfc1c48714921fc18dda83cda2
TasyDev <tamayocontat@gmail.com>
2025-11-21 21:51:25 -0500

    Add initial app.js with rules of proyect
    Created app.js file containing documentation for user login rules, outlining authentication requirements and expected program behavior.

---
+++ app.js
@@ -0,0 +1,9 @@
+/*
+RULES:
+
+1. The user can log in with a username and password.
+
+2. If the username and password are correct, the program should show a welcome message and the user’s timeline.
+
+3. If they aren’t correct, the program should say so.
+*/
